5ef6cd2186192acf451c00335c2c03656af0c7cf
boudewijn <boudewijn00@gmail.com>
2013-04-25 00:16:53 +0200

    add donate list and customer params for transaction

--- application/modules/default/controllers/DonateController.php
+++ application/modules/default/controllers/DonateController.php
@@ -17,7 +17,7 @@ class DonateController extends Kwgl_Controller_Action {
                 $linkedInId = $_SESSION["linked_in_id"];
                 
                 // get user id based on session linkedinid
-                $userData = $daoUser->fetchDetail(array("id"), array("linkedInId = ?" => $linkedInId));
+                $userData = $daoUser->fetchDetail(array("id","firstName","lastName","emailAddress"), array("linkedInId = ?" => $linkedInId));
                 $userId = $userData->id;
                 
                 Braintree_Configuration::environment('sandbox');
@@ -30,7 +30,13 @@ class DonateController extends Kwgl_Controller_Action {
                     'creditCard' => array(
                         'number' => '5105105105105100',
                         'expirationDate' => '05/12'
-                    )
+                    ), 
+                    'customer' => array(
+                        'firstName' => $userData->firstName,
+                        'lastName' => $userData->lastName,
+                        'email' => $userData->emailAddress,
+                        'id' => "1234"
+                  ),
                 ));
                 
                 $donationData["money"] = $money;
@@ -55,5 +61,55 @@ class DonateController extends Kwgl_Controller_Action {
             }
             
 	}
+        
+        public function succesfullAction(){
+            
+            
+            
+        }
+        
+        public function listAction(){
+            
+            require_once 'Braintree/Braintree.php';
+            
+            Braintree_Configuration::environment('sandbox');
+            Braintree_Configuration::merchantId('x2cz2ktbj4ywcntv');
+            Braintree_Configuration::publicKey('rsc6rj5dkxfsvqhm');
+            Braintree_Configuration::privateKey('6f9417d781216e146c36a15f8350b950');
+
+            $result = Braintree_Transaction::search(array( Braintree_TransactionSearch::type()->is(Braintree_Transaction::SALE)));
+
+            
+            
+            //Zend_Debug::dump($result);
+            $donations = array();
+            foreach($result->_ids AS $transactionId){
+                
+                //echo $transactionId."<br/>";
+                $transaction = Braintree_Transaction::find($transactionId);
+                
+                
+                //echo $transaction->_attributes["status"];
+                
+                if($transaction->_attributes["status"] == "authorized"){
+                
+                    //echo "email ".$transaction->_attributes["customer"]["email"];
+                    
+                    if($transaction->_attributes["customer"]["email"]){
+                        
+                        //Zend_Debug::dump($transaction);
+                        
+                        $donations[$transaction->_attributes["customer"]["email"]]["amount"] += $transaction->amount;
+                
+                    }
+                    
+                }
+            }
+            
+            Zend_Debug::dump($donations);
+            
+            die();
+            
+        }
 
 }
